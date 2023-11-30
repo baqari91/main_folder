b89602596f37bcf513811e95cd934ad700433aa0
baqari1991 <137100801+baqari1991@users.noreply.github.com>
2023-12-01 03:00:37 +0400

    done

--- classwork/class_work_20.py
+++ classwork/class_work_20.py
@@ -1,4 +1,87 @@
-import json
-with open('states.json') as file_state:
-    data = json.load(file_state)
-print(data)
+
+from time import time
+
+def execution_time_and_count_attributes(func):
+    def wrapper(self, *args, **kwargs):
+        start_time = time()
+
+        result = func(self, *args, **kwargs)
+
+        end_time = time()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time} seconds")
+
+        return result
+    return wrapper
+
+class Brand:
+    def __get__(self, instance, owner):
+        return instance._brand
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            instance._brand = value
+        else:
+            raise ValueError('Brand must be a string.')
+        
+class Model:
+    def __get__(self, instance, owner):
+        return instance._model
+    
+    def __set__(self, instance, value):
+        if isinstance(value, str):
+            instance._model = value
+        else:
+            raise ValueError('Model must be a string')
+
+class Year:
+    def __get__(self, instance, owner):
+        return instance._year
+    
+    def __set__(self, instance, value):
+        if isinstance(value, int):
+            instance._year = value
+        else:
+            raise ValueError('Year must be an integer')
+
+class Car:
+    brand = Brand()
+    model = Model()
+    year = Year()
+    
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self.year = year 
+    
+    @execution_time_and_count_attributes
+    def count_attributes(self):
+        attributes = ''
+        count = len(vars(self))
+        for key, _ in vars(self).items():
+            attributes += f'{key}, '
+        attributes = attributes[:-2] + '.'
+                
+        return f'Number of attributes: {count}.\nAttributes names: {attributes}'
+
+# Test the Car class
+car = Car('BMW', 'M4', 2023)
+result = car.count_attributes()
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import json
+# with open('states.json') as file_state:
+#     data = json.load(file_state)
+# print(data)
--- homework/bakar_tchumburidze_homework_17.py
+++ homework/bakar_tchumburidze_homework_17.py
@@ -1,24 +1,85 @@
-#1.დაწერეთ პითონის Car(ატრიბუტები:brand,model,year) კლასი და მახდინეთ ამკლასისთვის __new__ და __init__ მეთოდის გადაფარვა.
-class Car:
-    def __new__(cls,b,m,y):
-        instance = super().__new__(cls)
-        print("Creating a new instance of Car")
-        print(instance)
-        return instance
 
+from time import time
+
+def execution_time_and_count_attributes(func):
+    def wrapper(a):
+        start_time = time()
+
+        result = func(a)
+
+        end_time = time()
+        execution_time = end_time - start_time
+        print(f"Execution time: {execution_time} seconds")
+
+        return result
+    return wrapper
+
+class Brand:
+    def __get__(self, instance, owner):
+         return instance._brand
+    def __set__(self, instance, value):
+        if isinstance(value,str):
+            instance._brand = value
+            print('s')
+        else:
+            raise ValueError('brand must be a string.')
+        
+class Model:
+    def __get__(self, instance, owner):
+        return instance._model
+    def __set__(self, instance, value):
+        if isinstance(value,str):
+            instance._model = value
+        else:
+            raise ValueError('Model ,must be a string')
+
+class Year:
+    def __get__(self, instance, owner):
+        return instance._year
+    def __set__(self, instance, value):
+        if isinstance(value,int):
+            instance._year = value
+        else:
+            raise ValueError('Year must be a integer')
+
+class Car:
+    brand = Brand()
+    model = Model()
+    year = Year()
+    def __new__(cls, *args, **kwargs):
+        instance = super().__new__(cls)
+        print("print somthing until go init method")
+        return instance
+    # self-ში გადაეცემა ობიექტი, ზევით შექმნილი instance
     def __init__(self, brand, model, year):
         print("Initializing Car instance")
+
         self.brand = brand
         self.model = model
-        self.year = year
-        print(self)
+        self.year = year 
 
-# Creating an instance of Car
-car = Car("Toyota", "Corolla", 2023)
-print(f"Car: {car.brand} {car.model} ({car.year})")
+    @execution_time_and_count_attributes
+    def count_attributes(self):
+        attributes = ''
+        count = len(vars(self))
+        for key, _ in vars(self).items():
+                attributes += f'{key}, '
+        attributes = attributes[:-2] + '.'
+                
+        return f'Number of attributes: {count}.\nAttributes names: {attributes}'
 
 
+car1 = Car("Toyota", "Corolla", 2023)
+print(f"Car: {car1.brand} {car1.model} ({car1.year})")
+# # car1.brand = 3
 
+result = car1.count_attributes()
+print(result)
+# 
+print(car1.brand)
+
+
+print(car1.brand)
 
 #2.შექმენით დეკორატორი, რომელიც დაითვლის და დაგვიბეჭდავს დროს, თუ რა დრო დასჭრიდა ობიექტის მეთოდის შერულებას და დაგვიწერს რა ატრიბუტები აქვს გადმოცემული ობიექტს
-#3.შექმენით დეკორატორი, რომელიც დაითვლის და დაგვიბეჭდავს დროს, თუ რა დრო დასჭირდა ობიექტის მეთოდის შესრულებას და დაგვიწერს რა ატრიბუტები აქვს გადმოცემული ობიექს
+#3.Car კლასს დაუმატეთ თითოეული ატრიბუტისთვის set და get ფუნქტორი მათი ცვლილებებისთვის და დაამატეთ ვალიდაციები თითოეული ატრიბუტისთვის
