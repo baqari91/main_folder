a83c4a4376171f5400293088bd2e784a957de4de
baqari1991 <137100801+baqari1991@users.noreply.github.com>
2023-12-15 01:48:59 +0400

    14 dec

--- acali/home/bakar_tchumburidze_homework_chadzirobana_by_opp.py
+++ acali/home/bakar_tchumburidze_homework_chadzirobana_by_opp.py
@@ -1,47 +1,50 @@
 from random import randint, choice
 
+
 class Ship:
     def __init__(self, name, positions):
         self.name = name
         self.positions = positions
         self.destroyed = [False] * len(positions)
 
+
 class Game:
-    computer_positions1 = [
-        [Ship("Maverick", [3])], [Ship("Gunner", [8, 9])], [Ship("Matilda", [13, 14, 15])],
-        [Ship("Maverick", [4])], [Ship("Gunner", [10, 11])], [Ship("Matilda", [16, 17, 18])],
-        [Ship("Maverick", [5])], [Ship("Gunner", [12, 13])], [Ship("Matilda", [18, 19, 20])]
+    probable_computer_positions = [
+        [Ship("Maverick", [3]), Ship("Gunner", [8, 9]), Ship("Matilda", [13, 14, 15])],
+        [Ship("Maverick", [4]), Ship("Gunner", [10, 11]), Ship("Matilda", [16, 17, 18])],
+        [Ship("Maverick", [5]), Ship("Gunner", [12, 13]), Ship("Matilda", [18, 19, 20])]
     ]
-    computer = choice(computer_positions1)
+
     def __init__(self):
-        self.computer_positions = [
-            self.computer
-        ]
+        self.computer_positions = []
         self.player_positions = []
         self.setup_player_ships()
         self.game_playing = True
         self.player_destroyed_ship = 0
         self.computer_destroyed_ship = 0
+        self.generate_random_ships()
 
-        print(self.computer_positions)
+    def generate_random_ships(self):
+        random_computer_positions = choice(Game.probable_computer_positions)
+        self.computer_positions.extend(random_computer_positions)
 
     def setup_player_ships(self):
         ship_positions = [
-            (input("Ship Name: "), int(input("Position: "))),
-            (input("Medium Ship Name: "), int(input("Start position, add next number: "))),
-            (input("Big Ship Name: "), int(input("Start position, add next 2 numbers: ")))
+            (input("Ship Name: "), [int(input("Position: "))]),
+            (input("Medium Ship Name: "), list(map(int, input("Enter positions as a range (e.g., 4-5): ").split("-")))),
+            (input("Big Ship Name: "), list(map(int, input("Enter positions as a range (e.g., 7-9): ").split("-")))),
         ]
-        for _, positions in ship_positions:
-            self.player_positions.append(positions if isinstance(positions, list) else [positions])
-        print(ship_positions)
-    def check_ship_hit(self, ship_positions, target):
+        for name, positions in ship_positions:
+            self.player_positions.append(positions)
+
+    @staticmethod
+    def check_ship_hit(ship_positions, target):
         for idx, positions in enumerate(ship_positions):
             if target in positions:
                 ship_positions[idx][positions.index(target)] = "x"
 
                 return True
         return False
-
 
     def play_game(self):
         while self.game_playing:
@@ -55,13 +58,13 @@ class Game:
                 print(f"You hit the computer at position: {player_shoot}")
                 self.computer_destroyed_ship += 1
 
-
-            if self.player_destroyed_ship == 3:
-                self.game_playing = False
-                print("Congratulations! You won!")
-            if self.computer_destroyed_ship == 3:
+            if self.player_destroyed_ship == 6:
                 self.game_playing = False
                 print("Computer wins!")
+            if self.computer_destroyed_ship == 6:
+                self.game_playing = False
+                print("Congratulations! You won!")
+
 
 if __name__ == "__main__":
     game = Game()
--- acali/home/testing.py
+++ acali/home/testing.py
@@ -1,65 +1,20 @@
-from random import randint, choice
+from random import choice
 
 
 class Ship:
     def __init__(self, name, positions):
         self.name = name
         self.positions = positions
-        self.destroyed = [False] * len(positions)
+    def __repr__(self):
+        return f'{self.name}, {self.positions}'
 
 
-class Game:
-    def __init__(self):
-        self.computer_positions = [
-            Ship("Maverick", [3]),
-            Ship("Gunner", [8, 9]),
-            Ship("Matilda", [13, 14, 15])
-        ]
-        self.player_positions = []
-        self.setup_player_ships()
-        self.game_playing = True
-        self.player_destroyed_ship = 0
-        self.computer_destroyed_ship = 0
+probable_computer_positions = [
+        [Ship("Maverick", [3]), Ship("Gunner", [8, 9]), Ship("Matilda", [13, 14, 15])],
+        [Ship("Maverick", [4]), Ship("Gunner", [10, 11]), Ship("Matilda", [16, 17, 18])],
+        [Ship("Maverick", [5]), Ship("Gunner", [12, 13]), Ship("Matilda", [18, 19, 20])]
+    ]
+computer = choice(probable_computer_positions)
+print(computer)
 
-    def setup_player_ships(self):
-        ship_positions = [
-            (input("Ship Name: "), int(input("Position: "))),
-            (input("Medium Ship Name: "), int(input("Start position, add next number: "))),
-            (input("Big Ship Name: "), int(input("Start position, add next 2 numbers: ")))
-        ]
-        for _, positions in ship_positions:
-            self.player_positions.append(positions if isinstance(positions, list) else [positions])
-        print(ship_positions)
-
-    def check_ship_hit(self, ship_positions, target):
-        for idx, positions in enumerate(ship_positions):
-            if target in positions:
-                ship_positions[idx][positions.index(target)] = "x"
-                print('1', ship_positions)
-                # print('2', ship_positions[idx][positions.index(target)])
-                return True
-        return False
-
-    def play_game(self):
-        while self.game_playing:
-            computer_shoot = randint(1, 20)
-            if self.check_ship_hit(self.player_positions, computer_shoot):
-                print(f"Computer hit at position: {computer_shoot}")
-                self.player_destroyed_ship += 1
-
-            player_shoot = int(input("Enter position to fire (1-20): "))
-            if self.check_ship_hit([ship.positions for ship in self.computer_positions], player_shoot):
-                print(f"You hit the computer at position: {player_shoot}")
-                self.computer_destroyed_ship += 1
-
-            if self.player_destroyed_ship == 3:
-                self.game_playing = False
-                print("Congratulations! You won!")
-            if self.computer_destroyed_ship == 3:
-                self.game_playing = False
-                print("Computer wins!")
-
-
-if __name__ == "__main__":
-    game = Game()
-    game.play_game()
+help(computer.extend)
